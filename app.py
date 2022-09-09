942ce3738454742576d97f70423b5854eea41e33
田島和樹 <kazukitashima@tajimakazukinoMacBook-Air.local>
2022-09-10 08:29:09 +0900

    login機能の実装

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from datetime import datetime
 import pytz
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
  
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager() 
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(50), nullable=False, unique=True)
+	password = db.Column(db.String(25))
 
 class BlogArticle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,12 +32,51 @@ class BlogArticle(db.Model):
  
 @app.route('/', methods=['GET'])
 def blog():
-    if request.method == 'GET':
-        # DBに登録されたデータをすべて取得する
-        blogarticles = BlogArticle.query.all()
-        return render_template('index.html', blogarticles=blogarticles)
+    #ユーザーがログインしていれば
+    if current_user.is_authenticated:
+        if request.method == 'GET':
+            # DBに登録されたデータをすべて取得する
+            blogarticles = BlogArticle.query.all()
+            return render_template('index.html', blogarticles=blogarticles)
+    else:
+        return redirect('/login')
+
+#ユーザー登録
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Userのインスタンスを作成
+        user = User(username=username, password=generate_password_hash(password, method='sha256'))
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    else:
+        return render_template('signup.html')
+
+#ログイン
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Userテーブルからusernameに一致するユーザを取得
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
 
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == "POST":
         title = request.form.get('title')
