6e5400b0cce667eba529bbf97eb311385e4ace78
田島和樹 <kazukitashima@tajimakazukinoMacBook-Air.local>
2022-09-10 07:12:01 +0900

    ブログ作成(createまで)

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class BlogArticle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+ 
+@app.route('/', methods=['GET'])
+def blog():
+    if request.method == 'GET':
+        # DBに登録されたデータをすべて取得する
+        blogarticles = BlogArticle.query.all()
+        return render_template('index.html', blogarticles=blogarticles)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == "POST":
+        title = request.form.get('title')
+        body = request.form.get('body')
+        # BlogArticleのインスタンスを作成
+        blogarticle = BlogArticle(title=title, body=body)
+        db.session.add(blogarticle)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('create.html')
