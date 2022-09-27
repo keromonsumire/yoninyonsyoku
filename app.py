5632aa5f0d40e7b639e01c280278667634a15f9a
田島和樹 <kazukitashima@tajimakazukinoMacBook-Air.local>
2022-09-27 20:56:24 +0900

    db_uriのスペルミスを修正

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
 #db_uri = "postgresql://dorfwistzafixt:163331f2a38a515b9e82f4b07e0b1578d4ce8fd450c7706321fb824061908c0f@ec2-44-193-178-122.compute-1.amazonaws.com:5432/d77dv846539911"
-db_url = os.environ.get("DB_URL")
-if not db_url:
-    print("DB_URLを設定してください")
+db_uri = os.environ.get("DB_URI")
+if not db_uri:
+    print("DB_URIを設定してください")
     sys.exit(1)
 secret_key = os.environ.get("SECRET_KEY")
 if not secret_key:
