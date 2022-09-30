3f686bf35a61a1a61f45f014c2e7431b04fdbe47
田島和樹 <kazukitashima@tajimakazukinoMacBook-Air.local>
2022-09-30 16:33:39 +0900

    +9時間に設定

--- app.py
+++ app.py
@@ -333,8 +333,9 @@ def create():
                     return render_template('create.html')
         else:
             # BlogArticleのインスタンスを作成
-            JST = timezone(timedelta(hours=+9), 'JST')
-            blogarticle = BlogArticle(title=title, user_id=current_user.id, created_at=datetime.now(JST))
+            now = datetime.now()
+            JST = now + timedelta(hours=9)
+            blogarticle = BlogArticle(title=title, user_id=current_user.id, created_at=JST)
             db.session.add(blogarticle)
             db.session.commit()
 
@@ -697,8 +698,9 @@ def show_article(id):
 
     else:
         comment = request.form.get('comment')
-        JST = timezone(timedelta(hours=+9), 'JST')
-        comment_instance = Comment(blog_id = id, contributor_id = current_user.id, text = comment, created_at=datetime.now(JST))
+        now = datetime.now()
+        JST = now + timedelta(hours=9)
+        comment_instance = Comment(blog_id = id, contributor_id = current_user.id, text = comment, created_at=JST)
         db.session.add(comment_instance)
         db.session.commit()
 
